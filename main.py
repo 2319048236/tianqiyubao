6da75e54cdc7d10ee6a63c9aa21ec1bdcd5b6658
2319048236 <111738358+2319048236@users.noreply.github.com>
2022-09-11 23:52:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,15 +37,16 @@ birthday2 = birthday1.to_solar_date()#转化成公历日期，输出为字符串
 #为读取星座准备
 birthday3 = LunarDate(x, y, z)#构建农历生日日期
 birthday4 = birthday3.to_solar_date()
-cmonth = birthday4[5:7]
-cdate = birthday4[8:]
+cbir=birthday4.split('-') 
+cmonth=str(cbir[1]) 
+cdate=str(cbir[2])
 sdate=[20,19,21,20,21,22,23,23,23,24,23,22]
 conts =['摩羯座','水瓶座','双鱼座','白羊座','金牛座','双子座','巨蟹座','狮子座','处女座','天秤座','天蝎座','射手座','摩羯座']
 def sign(cmonth,cdate):  
-  if cdate < sdate[cmonth-1]:   # 如果日数据早于对应月列表中对应的日期
-      print(conts[cmonth-1])       # 直接输出星座列表对应月对应的星座
+  if int(cdate)<sdate[int(cmonth)-1]:   # 如果日数据早于对应月列表中对应的日期
+      print(conts[int(cmonth)-1])       # 直接输出星座列表对应月对应的星座
   else:
-      print(conts[cmonth])
+      print(conts[int(cmonth)])
   return sign
 
 if app_id is None or app_secret is None:
