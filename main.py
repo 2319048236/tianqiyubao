21eb981ca1e61696977942c3b89cd2606a9b2278
2319048236 <111738358+2319048236@users.noreply.github.com>
2022-09-12 15:54:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -43,10 +43,9 @@ sdate=[20,19,21,20,21,22,23,23,23,24,23,22]
 conts =['摩羯座','水瓶座','双鱼座','白羊座','金牛座','双子座','巨蟹座','狮子座','处女座','天秤座','天蝎座','射手座','摩羯座']
 def sign(cmonth,cdate):  
   if int(cdate)<sdate[int(cmonth)-1]:   # 如果日数据早于对应月列表中对应的日期
-      print(conts[int(cmonth)-1])       # 直接输出星座列表对应月对应的星座
+      return conts[int(cmonth)-1]       # 直接输出星座列表对应月对应的星座
   else:
-      print(conts[int(cmonth)])
-  return sign(cmonth,cdate)
+      return conts[int(cmonth)]
 
 if app_id is None or app_secret is None:
   print('请设置 APP_ID 和 APP_SECRET')
