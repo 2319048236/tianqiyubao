b4d6a8a2b1435fa82b92f7eee1b37b76961ad06b
2319048236 <111738358+2319048236@users.noreply.github.com>
2022-09-23 16:46:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -284,7 +284,7 @@ except WeChatClientException as e:
 wm = WeChatMessage(client)
 alarm1, aqi, win, win_speed, tem, tem1, tem2 = get_weather_1()
 #week, sunrise, sunset, weather, pop = get_weather_2()
-Day_1, Day_2, Day_3, Day_4, Day_5, Day_6,Ultraviolet, jianfei, xuetang, dressing, xiche, air_pollution = get_weather_3()
+Day_1, Day_2, Day_3, Day_4, Day_5, Day_6, Ultraviolet, jianfei, xuetang, dressing, xiche, air_pollution = get_weather_3()
 lubarmonth, lunarday, jieqi, lunar_festival, festival = get_lunar_calendar()
 lucky, finances, shuzi, aiqing, gongzuo, jiankang, guiren, gaishu = get_xingzuo()
 sure_new_loc, sure_new_hid, present, danger1, danger2 = get_Covid_19()
