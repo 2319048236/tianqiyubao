916a34ccab28e704cd8197f39356da50f20407bd
小九 <111738358+2319048236@users.noreply.github.com>
2022-11-11 11:04:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def get_weather_3():
     #res31 = xmltodict.parse(res3.text)['resp']
     res31 = res3.json()['data']
     res311 = res31[0]['index']
-    return res31[1]['wea'],res31[2]['wea'],res31[3]['wea'],res31[4]['wea'],res31[5]['wea'],res31[6]['wea'],res311[0]['desc'],res311[1]['desc'],res311[2]['desc'],res311[3]['desc'],res311[4]['desc'],res311[5]['desc'],res31[0]['week'], res31[0]['sunrise'], res31[0]['sunset'], res31[0]['wea'],
+    return res31[1]['wea'],res31[2]['wea'],res31[3]['wea'],res31[4]['wea'],res31[5]['wea'],res31[6]['wea'],res311[0]['desc'],res311[1]['desc'],res311[2]['desc'],res311[3]['desc'],res311[4]['desc'],res311[5]['desc'],res31[0]['week'], res31[0]['sunrise'], res31[0]['sunset'], res31[0]['wea'],res31[0]['humidity']
 
 
 # 星座
@@ -272,7 +272,7 @@ except WeChatClientException as e:
 
 wm = WeChatMessage(client)
 alarm1, aqi, win, win_speed, tem, tem1, tem2 = get_weather_1()
-Day_1, Day_2, Day_3, Day_4, Day_5, Day_6, Ultraviolet, jianfei, xuetang, dressing, xiche, air_pollution,week, sunrise, sunset, weather = get_weather_3()
+Day_1, Day_2, Day_3, Day_4, Day_5, Day_6, Ultraviolet, jianfei, xuetang, dressing, xiche, air_pollution,week, sunrise, sunset, weather,humidity = get_weather_3()
 lubarmonth, lunarday, jieqi, lunar_festival, festival = get_lunar_calendar()
 lucky, finances, shuzi, aiqing, gongzuo, jiankang, guiren, gaishu = get_xingzuo()
 sure_new_loc, sure_new_hid, present, danger1, danger2 = get_Covid_19()
@@ -334,7 +334,7 @@ data = {
         "color": get_random_color()
     },
     "b": {
-        "value": "%",
+        "value": humidity,
         "color": get_random_color()
     },
     "c": {
