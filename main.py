dc2709a742fe28dbd096aa9ddbe6ceed2902856a
小九 <111738358+2319048236@users.noreply.github.com>
2022-11-11 09:50:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -79,16 +79,6 @@ def get_weather_1():
     res11 = res1.json()
     return res11['alarm'], res11['aqi'], res11['win'], res11['win_speed'], res11['tem'], res11['tem2'], res11['tem1']
 
-# 天行数据接口
-def get_weather_2():
-    url = "http://api.tianapi.com/tianqi/index?key=d5edced4967c76fd11899dbe1b753d91&city=" + city
-    res2 = requests.get(url, verify=False)
-    if res2.status_code != 200:
-        return res2
-    res21 = res2.json()['newslist'][0]
-    return res21['week'], res21['sunrise'], res21['sunset'], res21['weather']
-
-
 def get_weather_3():
     url = "http://www.tianqiapi.com/api?version=v1&appid=78158848&appsecret=650ylFRx&city=" + city
     res3 = requests.get(url)
@@ -282,8 +272,7 @@ except WeChatClientException as e:
 
 wm = WeChatMessage(client)
 alarm1, aqi, win, win_speed, tem, tem1, tem2 = get_weather_1()
-week, sunrise, sunset, weather = get_weather_2()
-Day_1, Day_2, Day_3, Day_4, Day_5, Day_6, Ultraviolet, jianfei, xuetang, dressing, xiche, air_pollution,week, sunrise, sunset, wea = get_weather_3()
+Day_1, Day_2, Day_3, Day_4, Day_5, Day_6, Ultraviolet, jianfei, xuetang, dressing, xiche, air_pollution,week, sunrise, sunset, weatherther = get_weather_3()
 lubarmonth, lunarday, jieqi, lunar_festival, festival = get_lunar_calendar()
 lucky, finances, shuzi, aiqing, gongzuo, jiankang, guiren, gaishu = get_xingzuo()
 sure_new_loc, sure_new_hid, present, danger1, danger2 = get_Covid_19()
