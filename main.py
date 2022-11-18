50577c88cb44025fdf998d3ac2630635d72e1d8d
小九 <111738358+2319048236@users.noreply.github.com>
2022-11-19 01:11:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -318,7 +318,7 @@ data = {
         "color": get_random_color()
     },
     "a": {
-        "value": win[0] + win_speed,
+        "value": win[0],
         "color": get_random_color()
     },
     "b": {
